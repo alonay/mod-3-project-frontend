ade97524b4a9d1dd0d4e154b38496db8f8d7f480
github username <github email address>
2017-12-04 17:38:03 -0500

    list of all contestants

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-document.addEventListener("DOMContentLoaded", function(){
-     function getShows(){
-           fetch('http://localhost:3000/shows')
-             .then(res => res.json())
-             .then(contestants => console.log(contestants))
-           }
-
-let prediction = document.getElementById("prediction_input")
-let predictions = document.getElementById("prediction_history")
-let form_submission = document.getElementById("prediction_form")
-//let button_submit = document.getElementById("button_submit")
-
-document.getElementById("button_submit").addEventListener("click", ()=> {
-
-
-  let input = document.getElementById("prediction_input")
-  predictions.innerHTML += `<li>${input.value}</li>`
-})
-})
+// document.addEventListener("DOMContentLoaded", function(){
+//      function getShows(){
+//            fetch('http://localhost:3000/shows')
+//              .then(res => res.json())
+//              .then(contestants => console.log(contestants))
+//            }
+//
+// let prediction = document.getElementById("prediction_input")
+// let predictions = document.getElementById("prediction_history")
+// let form_submission = document.getElementById("prediction_form")
+// //let button_submit = document.getElementById("button_submit")
+//
+// document.getElementById("button_submit").addEventListener("click", ()=> {
+//
+//
+//   let input = document.getElementById("prediction_input")
+//   predictions.innerHTML += `<li>${input.value}</li>`
+// })
+// })
 
 
 // getContestants()
