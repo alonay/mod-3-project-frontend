d74f10eedad93116b872fe3689d65c52727d2f5e
Alona <github email address>
2017-12-04 10:52:18 -0500

    added button

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(){
      function getBooks(){
-           fetch('https://dog.ceo/api/breeds/list/all')
+           fetch('')
              .then(res => res.json())
-             .then(console.log)
+             .then(contestants => getContestants(contestants))
+           }
+
+let prediction = document.getElementById("prediction_input")
+let predictions = document.getElementById("prediction_history")
+let form_submission = document.getElementById("prediction_form")
+
+prediction.addEventListener("click", (event) => {
+  event.preventDefault()
+  let input = document.getElementById("prediction_input")
+  predictions.innerHTML += `<li>${input.value} at ${time.now}</li>`
 }
 
-getBooks()
+
+
+getContestants()
