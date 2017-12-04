e3f2fdad78447fa6902439b87e8a45fd97596255
Alona <github email address>
2017-12-04 15:45:46 -0500

    new html

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(){
-     function getBooks(){
-           fetch('')
-             .then(res => res.json())
-             .then(contestants => getContestants(contestants))
-           }
+    //  function getContestants(){
+    //        fetch('')
+    //          .then(res => res.json())
+    //          .then(contestants => getContestants(contestants))
+    //        }
 
 let prediction = document.getElementById("prediction_input")
 let predictions = document.getElementById("prediction_history")
 let form_submission = document.getElementById("prediction_form")
+//let button_submit = document.getElementById("button_submit")
 
-prediction.addEventListener("click", (event) => {
-  event.preventDefault()
+document.getElementById("button_submit").addEventListener("click", ()=> {
+
+
   let input = document.getElementById("prediction_input")
-  predictions.innerHTML += `<li>${input.value} at ${time.now}</li>`
-}
+  predictions.innerHTML += `<li>${input.value}</li>`
+})
+})
 
 
-
-getContestants()
+// getContestants()
